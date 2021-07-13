7d87016fb40017f724d867f580006a6467717831
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2021-07-14 00:51:32 +0200

    new files

---
+++ .doom.d/custom.el
@@ -0,0 +1,22 @@
+;; Vacaciones personalizadas
+(setq holiday-other-holidays
+      '((holiday-fixed 1 6 "Día de Reyes")))
+
+;; La semana comienza el lunes
+(setq calendar-week-start-day 1)
+
+;; Configuración de org-journal
+(setq org-journal-dir "diario/"
+      org-journal-date-prefix "#+TITLE: "
+      org-journal-time-prefix "* "
+      org-journal-date-format "%a, %Y-%m-%d"
+      org-journal-file-format "%Y%m%d.org")
+
+;; Directorio de inicio al abrir emacs
+(setq default-directory "~/MEGA/org")
+
+;; Información relativa a org-capture
+;; Definir variables:
+;;   (defvar +org-capture-todo-file "personal.org")
+;;   (defvar +org-capture-notes-file "personal.org")
+;;   (defvar +org-capture-journal-file "personal.org")
