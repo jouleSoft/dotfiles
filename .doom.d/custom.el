6066b9660436afd3fe19d66f27a9d19caa306e7b
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2021-08-16 07:49:53 +0200

    authomatic backup

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -1,28 +1,20 @@
-;; Vacaciones personalizadas
+;; Custom holydays
 (setq holiday-other-holidays
       '((holiday-fixed 1 6 "Día de Reyes")))
 
-;; La semana comienza el lunes
+;; Monday as first of the week
 (setq calendar-week-start-day 1)
 
-;; Configuración de org-journal
+;; org-journal configuration
+;; open: <space> n j j
 (setq org-journal-dir "diario/"
       org-journal-date-prefix "#+TITLE: "
       org-journal-time-prefix "* "
       org-journal-date-format "%a, %Y-%m-%d"
       org-journal-file-format "%Y%m%d.org")
 
-;; Directorio de inicio al abrir emacs
+;; Set the default directory when Emacs starts
 (setq default-directory "~/MEGA/org")
-
-;; Default file for notes
-;; (setq org-default-notes-file (concat org-directory "/notes.org"))
-
-;; Información relativa a org-capture
-;; Definir variables:
-;;    (defvar +org-capture-todo-file "personal.org")
-;;    (defvar +org-capture-notes-file "personal.org")
-;;    (defvar +org-capture-journal-file "personal.org")
 
 ;; Indentation
 (setq-default tab-width 2)
