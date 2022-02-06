4929b48fc388374cf143fe772d768f08b798c21e
Julio Jiménez Delgado <julio.jimenezdelgado@gmail.com>
2022-02-06 13:48:06 +0100

    authomatic backup

--- .config/i3/scripts/dmUrl.sh
+++ .config/i3/scripts/dmUrl.sh
@@ -47,20 +47,20 @@ declare -a options=(
 "i3               | https://i3wm.org/docs/userguide.html"
 "i3status         | https://i3wm.org/i3status/manpage.html"
 "i3status-rust    | https://github.com/greshake/i3status-rust"
-"Cockney RS       | https://londontopia.net/londonism/fun-london/language-top-100-cockney-rhyming-slang-words-and-phrases/"
 "libvirt-Arch     | https://wiki.archlinux.org/title/Libvirt"
-"LPIC-1           | https://learning.lpi.org/en/learning-materials/101-500/"
 "mermaid          | https://mermaid-js.github.io/mermaid/#/Tutorials"
+"pihole           | http://172.29.0.2/admin"
 "polybar          | https://github.com/polybar/polybar/wiki"
 "qtile            | https://docs.qtile.org/en/latest/"
 "Shell Scripting  | https://devhints.io/bash"
+"sigma-syncthing  | https://10.13.13.3:8384"
+"sigma-cockpit    | https://10.13.13.3:9090"
 "vim-awesome      | https://vimawesome.com/"
 "vim-config       | https://vimconfig.com/"
 "vim-leaderkey    | https://tuckerchapman.com/2018/06/16/how-to-use-the-vim-leader-key/"
 "vim-plug         | https://github.com/junegunn/vim-plug"
 "wordreference    | https://www.wordreference.com/"
 "xrandr-config    | https://blog.stigok.com/2017/08/14/multimonitor-setup-with-i3-and-xrandr-on-arch-linux.html"
-"zabbix-es        | https://www.zabbix-es.com.es/index.php/Zabbix_de_0_a_100_-_Documentaci%C3%B3n"
 "quit"
 )
 
@@ -69,14 +69,14 @@ declare -a options=(
 #Script header
 header() 
 {
-	#Init color variables
-	NC='\033[0m'
-	LIGHT_GREY='\033[0;37m'
-	YELLOW='\033[1;33m'
+  #Init color variables
+  NC='\033[0m'
+  LIGHT_GREY='\033[0;37m'
+  YELLOW='\033[1;33m'
 
-	echo 
-	echo -e "${LIGHT_GREY} $script_name ${YELLOW}$version ${LIGHT_GREY}- $description${NC}\n"
-	echo 
+  echo 
+  echo -e "${LIGHT_GREY} $script_name ${YELLOW}$version ${LIGHT_GREY}- $description${NC}\n"
+  echo 
 }
 
 #Operational functions (if required)
@@ -84,23 +84,35 @@ header()
 #Main function
 main()
 {
-	# Piping options array into dmenu.
-	# We use "printf '%s\n'" to format the array one item to a line.
-	choice=$(printf '%s\n' "${options[@]}" | dmenu -i -l 20 -nf '#F8F8F2' -nb '#282A36' -sb '#6272A4' -sf '#F8F8F2' -p 'Open WWW:')
+  # Piping options array into dmenu.
+  # We use "printf '%s\n'" to format the array one item to a line.
+  choice=$(printf '%s\n' "${options[@]}" | dmenu -i -l 20 -nf '#F8F8F2' -nb '#282A36' -sb '#6272A4' -sf '#F8F8F2' -p 'Open WWW:')
 
-	# What to do when/if we choose 'quit'.
-	if [ "$choice" == "quit" ]; then
-		echo "Program terminated." && exit 1
+  # What to do when/if we choose 'quit'.
+  if [ "$choice" == "quit" ]; then
+    echo "Program terminated." && exit 1
 
-		# What to do when/if we choose a file to edit.
-	elif [ "$choice" ]; then
-		cfg=$(printf '%s\n' "${choice}" | awk '{print $NF}')
-		$NAV "$cfg"
+  elif [ "$choice" == "pihole           | http://172.29.0.2/admin" ]; then
+    cfg=$(printf '%s\n' "${choice}" | awk '{print $NF}')
+    firefox "$cfg"
 
-		# What to do if we just escape without choosing anything.
-	else
-		echo "Program terminated." && exit 1
-	fi
+  elif [ "$choice" == "sigma-syncthing  | https://10.13.13.3:8384" ]; then
+    cfg=$(printf '%s\n' "${choice}" | awk '{print $NF}')
+    firefox "$cfg"
+
+  elif [ "$choice" == "sigma-cockpit    | https://10.13.13.3:9090" ]; then
+    cfg=$(printf '%s\n' "${choice}" | awk '{print $NF}')
+    firefox "$cfg"
+
+  # What to do when/if we choose a file to edit.
+  elif [ "$choice" ]; then
+    cfg=$(printf '%s\n' "${choice}" | awk '{print $NF}')
+    $NAV "$cfg"
+
+  # What to do if we just escape without choosing anything.
+  else
+    echo "Program terminated." && exit 1
+  fi
 }
 
 #-------------------------------------------[Execution]--------------------------------------------
